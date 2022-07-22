c0ef935a0a4413657f48456aa9a9b5ee65fa8d5b
Saroj Rai <raisaroj360@gmail.com>
2022-07-23 04:42:40 +0545

    leetcode: 203 - remove linked list elements

---
+++ leetcode/data_structures/203.remove linked list elements.cpp
@@ -0,0 +1,46 @@
+// 203. Remove Linke List Elements
+// Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+// Example 1:
+// Input: head = [1,2,6,3,4,5,6], val = 6
+// Output: [1,2,3,4,5]
+// Example 2:
+// Input: head = [], val = 1
+// Output: []
+// Example 3:
+// Input: head = [7,7,7,7], val = 7
+// Output: []
+// Constraints:
+// The number of nodes in the list is in the range [0, 104].
+// 1 <= Node.val <= 50
+// 0 <= val <= 50
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode *prev = nullptr, *res = nullptr;
+        while(head) { 
+            if(head->val == val) { 
+                if(prev) { 
+                    prev->next = head->next; 
+                }
+                head = head->next;
+                continue;
+            } 
+            
+            prev = head;
+            if(!res) res = prev;
+            head = head->next;
+        }
+        return res;
+    }
+};
