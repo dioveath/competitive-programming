f4f674dff7a8efb99fa7f79000037a1dccbc84b8
Saroj Rai <raisaroj360@gmail.com>
2022-08-01 23:20:16 +0545

    leetcode: 283 - move zeros

---
+++ leetcode/algorithms/283. move zeros.cpp
@@ -0,0 +1,40 @@
+// 283. Move Zeroes
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
+ 
+
+// Example 1:
+
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [0]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// -231 <= nums[i] <= 231 - 1
+ 
+
+// Follow up: Could you minimize the total number of operations done?
+
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        vector<int> temp = nums;
+        for(int i = 0, j = 0; i < nums.size(); i++){
+            if(temp[i] == 0) { 
+                nums[nums.size() - j - 1] = 0;
+                j++;
+            } else {
+                nums[i-j] = temp[i];
+            }
+        }
+    }
+};
