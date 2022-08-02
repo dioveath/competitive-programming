8b45c45ab20f4cc8e3d66e369c37de309a0f05f0
Saroj Rai <raisaroj360@gmail.com>
2022-08-02 19:44:18 +0545

    leetcode: 344 - reverse string

---
+++ leetcode/algorithms/344. reverse string.cpp
@@ -0,0 +1,30 @@
+// 344. Reverse String
+// Write a function that reverses a string. The input string is given as an array of characters s.
+// You must do this by modifying the input array in-place with O(1) extra memory.
+
+// Example 1:
+
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+// Example 2:
+
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 105
+// s[i] is a printable ascii character.
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int len = s.size();
+        for(int i = 0; i < len/2; i++){
+            char temp = s[i];
+            s[i] = s[(len - 1) - i];
+            s[(len - 1) - i] = temp;
+        }
+    }
+};
