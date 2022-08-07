b1d876e41a909b2dd5994a34dccacbe58d9d2c70
Saroj Rai <raisaroj360@gmail.com>
2022-08-07 22:26:08 +0545

    leetcode: 994 - rotting oranges

--- leetcode/algorithms/502. 01 Matrix.cpp
+++ leetcode/algorithms/502. 01 Matrix.cpp
@@ -19,35 +19,28 @@
 
 class Solution {
 public:
-    vector<vector<int>> dp;
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        dp.resize(mat.size(), vector<int>(mat[0].size(), 100000));
+
+  vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+    vector<vector<int>> dp(mat.size(), vector<int>(mat[0].size(), 100000));    
         
-        for(int i = 0; i < mat.size(); i++){
-            for(int j = 0; j < mat[0].size(); j++){
-                if(mat[i][j] == 0) {
-                    dp[i][j] = 0;
-                    continue;
-                }
-                if(i > 0) dp[i][j] = min(dp[i][j], dp[i-1][j] + 1);                
-                if(j > 0) dp[i][j] = min(dp[i][j], dp[i][j-1] + 1);
-            }
-        }
-        
-        for(int i = mat.size() - 1; i >= 0; i--){
-            for(int j = mat[0].size() - 1; j >= 0; j--){
-                if(mat[i][j] == 0) {
-                    dp[i][j] = 0;
-                    continue;
-                }
-                if(i < mat.size() - 1) dp[i][j] = min(dp[i][j], dp[i+1][j] + 1);                
-                if(j < mat[0].size() - 1) dp[i][j] = min(dp[i][j], dp[i][j+1] + 1);
-            }
-        }
-        
-        
-        return dp;
+    for(int i = 0; i < mat.size(); i++){
+      for(int j = 0; j < mat[0].size(); j++){
+	if(mat[i][j] == 0) { dp[i][j] = 0; continue; }
+	if(i > 0) dp[i][j] = min(dp[i][j], dp[i-1][j] + 1);                
+	if(j > 0) dp[i][j] = min(dp[i][j], dp[i][j-1] + 1);
+      }
     }
+        
+    for(int i = mat.size() - 1; i >= 0; i--){
+      for(int j = mat[0].size() - 1; j >= 0; j--){
+	if(mat[i][j] == 0) { dp[i][j] = 0; continue; }
+	if(i < mat.size() - 1) dp[i][j] = min(dp[i][j], dp[i+1][j] + 1);                
+	if(j < mat[0].size() - 1) dp[i][j] = min(dp[i][j], dp[i][j+1] + 1);
+      }
+    }
+        
+    return dp;
+  }
     
 };
 
