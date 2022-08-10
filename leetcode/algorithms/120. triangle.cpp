58071ce5ec607799cda0eda04c9c34487be42e0c
Saroj Rai <raisaroj360@gmail.com>
2022-08-11 01:55:51 +0545

    leetcode: 2287 - rearrange characters to make target string

--- leetcode/algorithms/120. triangle.cpp
+++ leetcode/algorithms/120. triangle.cpp
@@ -2,8 +2,6 @@
 // Given a triangle array, return the minimum path sum from top to bottom.
 // For each step, you may move to an adjacent number of the row below. More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
 
- 
-
 // Example 1:
 
 // Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
