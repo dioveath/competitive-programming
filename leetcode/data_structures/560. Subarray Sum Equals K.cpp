45d93fe846810840d22ddbb2e0c538fee07c7c7b
Saroj Rai <raisaroj360@gmail.com>
2022-09-10 21:05:31 +0545

    leetcode: 560 - subarray sum equals k

---
+++ leetcode/data_structures/560. Subarray Sum Equals K.cpp
@@ -0,0 +1,33 @@
+// 560. Subarray Sum Equals K
+// Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+// A subarray is a contiguous non-empty sequence of elements within an array.
+// Example 1:
+
+// Input: nums = [1,1,1], k = 2
+// Output: 2
+// Example 2:
+
+// Input: nums = [1,2,3], k = 3
+// Output: 2
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 2 * 104
+// -1000 <= nums[i] <= 1000
+// -107 <= k <= 107
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int sum = 0, cnt = 0;
+        int dp[nums.size()], p = 0;
+        for(int i = 0; i < nums.size(); i++){
+            sum += nums[i];
+            if(sum == k) cnt++;
+            for(int j = 0; j < p; j++) if(sum - dp[j] == k) cnt++;
+            dp[p++] = sum;
+        }
+        return cnt;
+    }
+};
