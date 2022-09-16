c0fdd66ace40370070254e11f89395619eab2106
Saroj Rai <raisaroj360@gmail.com>
2022-09-16 23:53:05 +0545

    leetcode: 108 - convert sorted array to binary search tree

---
+++ leetcode/data_structures/108. Convert Sorted Array to Binary Search Tree.cpp
@@ -0,0 +1,51 @@
+// 108. Convert Sorted Array to Binary Search Tree
+// Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+// A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+// Example 1:
+
+
+// Input: nums = [-10,-3,0,5,9]
+// Output: [0,-3,9,-10,null,5]
+// Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+// Example 2:
+
+
+// Input: nums = [1,3]
+// Output: [3,1]
+// Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 104
+// -104 <= nums[i] <= 104
+// nums is sorted in a strictly increasing order.
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return array_bst(nums, 0, nums.size()-1, new TreeNode());
+    }
+    
+    TreeNode* array_bst(vector<int> nums, int l, int r, TreeNode* root){
+        if(l > r) return nullptr;
+        int m = l+(r-l)/2;
+        root->val = nums[m];
+        root->left = array_bst(nums, l, m-1, new TreeNode());
+        root->right = array_bst(nums, m+1, r, new TreeNode());
+        return root;
+    }
+};
