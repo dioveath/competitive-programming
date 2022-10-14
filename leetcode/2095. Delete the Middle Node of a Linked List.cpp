8aadff688eb940fca63efb66e04047fa8f469127
Saroj Rai <raisaroj360@gmail.com>
2022-10-15 00:50:25 +0100

    leetcode: 2095 - delete the middle node of a linked list

---
+++ leetcode/2095. Delete the Middle Node of a Linked List.cpp
@@ -0,0 +1,59 @@
+// 2095. Delete the Middle Node of a Linked List
+// You are given the head of a linked list. Delete the middle node, and return the head of the modified linked list.
+// The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start using 0-based indexing, where ⌊x⌋ denotes the largest integer less than or equal to x.
+// For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
+// Example 1:
+
+// Input: head = [1,3,4,7,1,2,6]
+// Output: [1,3,4,1,2,6]
+// Explanation:
+// The above figure represents the given linked list. The indices of the nodes are written below.
+// Since n = 7, node 3 with value 7 is the middle node, which is marked in red.
+// We return the new list after removing this node. 
+// Example 2:
+
+// Input: head = [1,2,3,4]
+// Output: [1,2,4]
+// Explanation:
+// The above figure represents the given linked list.
+// For n = 4, node 2 with value 3 is the middle node, which is marked in red.
+// Example 3:
+
+// Input: head = [2,1]
+// Output: [2]
+// Explanation:
+// The above figure represents the given linked list.
+// For n = 2, node 1 with value 1 is the middle node, which is marked in red.
+// Node 0 with value 2 is the only node remaining after removing node 1.
+
+// Constraints:
+
+// The number of nodes in the list is in the range [1, 105].
+// 1 <= Node.val <= 105
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        ListNode *sentinel = new ListNode(-1);
+        sentinel->next = head;        
+        ListNode* slow = sentinel, *fast = head;
+        
+        while(fast && fast->next){
+            fast = fast->next ? fast->next->next : fast->next;
+            slow = slow->next;
+        }
+        
+        slow->next = slow->next ? slow->next->next : slow->next;
+        return sentinel->next;
+    }
+};
