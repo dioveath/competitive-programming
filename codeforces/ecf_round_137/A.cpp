a86a89d97adb5f991f4d6a5172bd5ff6ecd062c2
Saroj Rai <raisaroj360@gmail.com>
2022-10-18 12:34:04 +0100

    leetcode: 38 - count and say

---
+++ codeforces/ecf_round_137/A.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+inline int fact(int n) {
+  if(n == 1) return 1;
+  return n * fact(n-1);
+} 
+
+
+void solve(int t){
+  int n;
+  cin >> n;
+
+  vector<int> a(n);
+  for(int i = 0; i < n; i++) cin >> a[i];
+
+  // int ans = 
+  cout << ans << endl;
+}
+
+
+int main(){
+  int T;
+  cin >> T;
+
+  for(int i = 0; i < T; i++)
+    solve(i+1);
+}
