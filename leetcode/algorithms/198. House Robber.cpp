9c8b00ae0c84436e35f198967656e4223ab1f9d5
Saroj Rai <raisaroj360@gmail.com>
2022-11-07 01:03:35 +0000

    leetcode: 45 - jump game ii

--- leetcode/algorithms/198. House Robber.cpp
+++ leetcode/algorithms/198. House Robber.cpp
@@ -20,6 +20,25 @@
 // 1 <= nums.length <= 100
 // 0 <= nums[i] <= 400
 
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        
+        vector<int> dp(n, 0);
+        dp[0] = nums[0], dp[1] = max(nums[0], nums[1]);
+
+        for(int i = 2; i < n; i++)
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i]);
+
+        return dp[n-1];
+    }
+};
+
+
+
 class Solution {
 public:
     int dp[100];
@@ -45,3 +64,5 @@ public:
     
 
 };
+
+
