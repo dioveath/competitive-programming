d2d103e0a799722349fab4636c801f0409ed32e2
Saroj Rai <raisaroj360@gmail.com>
2022-11-08 00:02:35 +0000

    leetcode: 198 - house robber updated

--- leetcode/algorithms/198. House Robber.cpp
+++ leetcode/algorithms/198. House Robber.cpp
@@ -22,6 +22,23 @@
 
 class Solution {
 public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        
+        vector<int> dp(n, 0);
+        dp[0] = nums[0], dp[1] = max(nums[0], nums[1]);
+
+        for(int i = 2; i < n; i++)
+            dp[i] = max(dp[i-1], dp[i-2]+nums[i]);
+
+        return dp[n-1];
+    }
+};
+
+
+class Solution {
+public:
     int dp[100];
     int rob(vector<int>& nums) {
         for(int i = 0; i < 100; i++) dp[i] = -1;
