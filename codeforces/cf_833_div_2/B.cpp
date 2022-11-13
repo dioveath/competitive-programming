63801964e91fbccb54b98b6619198cf723982d93
Saroj Rai <raisaroj360@gmail.com>
2022-11-13 23:51:46 +0000

    codeforces: cf 833 div 2 - B

--- codeforces/cf_833_div_2/B.cpp
+++ codeforces/cf_833_div_2/B.cpp
@@ -9,10 +9,20 @@ void solve(int t){
   string s;
   cin >> s;
 
+  int ans = 0;
   for(int i = 0; i < n; i++){
+    int freq[10] = {0};
+    int max_freq = 0, dist = 0;
+    for(int j = i; j < n && j < i+100; j++){
+      freq[s[j]-'0']++;
+      if(freq[s[j]-'0'] == 1) dist++;
+
+      max_freq = max(freq[s[j]-'0'], max_freq);
+      if(max_freq <= dist) ans++;
+    }
   }
 
-  cout << (n(n+1))/2 << endl;
+  cout << ans << endl;
 }
 
 
