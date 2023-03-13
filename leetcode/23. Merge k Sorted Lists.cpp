5cabc9392794f91c1c05680f0c7c81fccdd3a26b
Saroj Rai <raisaroj360@gmail.com>
2023-03-13 01:54:41 +0000

    leetcdoe: 23 - merge k sorted lists updated solution

--- leetcode/23. Merge k Sorted Lists.cpp
+++ leetcode/23. Merge k Sorted Lists.cpp
@@ -76,3 +76,75 @@ public:
         return res;
     }
 };
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        // given array of k linked-lists
+        // all linked lists are sorted in ascending order
+        // merge all the linked-lsits into one sorted linked-list
+        // [[ 1, 4, 5], [1, 3, 4], [2, 6]]
+        // [1, 1, 2, 3, 4, 4, 5, 6]
+
+        // [[1, 4, 5], [1, 3, 4]]
+        // iterate through both linked list simultanously
+        // we create a new node
+        // on each iteration
+        // we check which node is smaller of the current node
+        // we will add this our root->next node
+        // and then move the small node to next one
+
+        // we'll need to iterate and merge through all the remaining lists
+        // we can use divide and conquer
+        if(lists.size() == 0) return nullptr;
+        return divide(lists, 0, lists.size()-1);
+    }
+
+    ListNode* conquer(ListNode* start, ListNode* end){
+        ListNode *ans = new ListNode(0);
+        ListNode *root = ans;
+        while(start && end) {
+            if(start->val < end->val) {
+                root->next = start;
+                start = start->next;
+            } else {
+                root->next = end;
+                end = end->next;
+            }
+            root = root->next;
+        }
+        while(start) { 
+            root->next = start;
+            start = start->next;
+            root = root->next;
+        }
+        while(end) { 
+            root->next = end;
+            end = end->next;
+            root = root->next;
+        }
+        return ans->next;    
+    }
+
+    ListNode* divide(vector<ListNode*>& list, int start, int end){
+        int sz = (end - start)+1;
+        if(sz == 1) return list[start];
+        if(sz == 2) return conquer(list[start], list[end]); 
+        int half = sz/2;
+        ListNode* left = divide(list, start, start + half-1);
+        ListNode* right = divide(list, start + half, end);
+        return conquer(left, right);
+    }
+};
