d912f98ae55d23934c0fbf65f9157fe83afc5f4e
Saroj Rai <raisaroj360@gmail.com>
2023-03-30 03:13:58 +0100

    leetcode: 87 - scramble string

---
+++ leetcode/87. Scramble String.cpp
@@ -0,0 +1,65 @@
+// 87. Scramble String
+// We can scramble a string s to get a string t using the following algorithm:
+
+// If the length of the string is 1, stop.
+// If the length of the string is > 1, do the following:
+// Split the string into two non-empty substrings at a random index, i.e., if the string is s, divide it to x and y where s = x + y.
+// Randomly decide to swap the two substrings or to keep them in the same order. i.e., after this step, s may become s = x + y or s = y + x.
+// Apply step 1 recursively on each of the two substrings x and y.
+// Given two strings s1 and s2 of the same length, return true if s2 is a scrambled string of s1, otherwise, return false.
+
+ 
+
+// Example 1:
+
+// Input: s1 = "great", s2 = "rgeat"
+// Output: true
+// Explanation: One possible scenario applied on s1 is:
+// "great" --> "gr/eat" // divide at random index.
+// "gr/eat" --> "gr/eat" // random decision is not to swap the two substrings and keep them in order.
+// "gr/eat" --> "g/r / e/at" // apply the same algorithm recursively on both substrings. divide at random index each of them.
+// "g/r / e/at" --> "r/g / e/at" // random decision was to swap the first substring and to keep the second substring in the same order.
+// "r/g / e/at" --> "r/g / e/ a/t" // again apply the algorithm recursively, divide "at" to "a/t".
+// "r/g / e/ a/t" --> "r/g / e/ a/t" // random decision is to keep both substrings in the same order.
+// The algorithm stops now, and the result string is "rgeat" which is s2.
+// As one possible scenario led s1 to be scrambled to s2, we return true.
+// Example 2:
+
+// Input: s1 = "abcde", s2 = "caebd"
+// Output: false
+// Example 3:
+
+// Input: s1 = "a", s2 = "a"
+// Output: true
+ 
+
+// Constraints:
+
+// s1.length == s2.length
+// 1 <= s1.length <= 30
+// s1 and s2 consist of lowercase English letters.
+
+class Solution {
+public:
+    bool isScramble(string s1, string s2) {
+        int n = s1.length();
+        vector dp(n+1, vector(n, vector<int>(n)));
+
+        for(int i = 0; i < n; i++)
+          for(int j = 0; j < n; j++)
+            dp[1][i][j] = s1[i] == s2[j];
+
+        for(int len = 2; len <= n; len++){
+          for(int i = 0; i < n+1-len; i++){
+            for(int j = 0; j < n+1-len; j++){
+              for(int newLen = 1; newLen < len; newLen++){
+                dp[len][i][j] |= (dp[newLen][i][j] && dp[len-newLen][i+newLen][j+newLen]);
+                dp[len][i][j] |= (dp[newLen][i][j+len-newLen] && dp[len-newLen][i+newLen][j]);
+              }
+            }
+          }
+        }
+
+      return dp[n][0][0];     
+    }
+};
