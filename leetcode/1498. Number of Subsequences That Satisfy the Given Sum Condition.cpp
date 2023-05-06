3093fe5b1cc7b9ebd4d74049806d06cc29ad2c01
Saroj Rai <raisaroj360@gmail.com>
2023-05-06 02:33:21 +0100

    leetcode: 1498 - number of subsequences that satisfy the given sum condition

---
+++ leetcode/1498. Number of Subsequences That Satisfy the Given Sum Condition.cpp
@@ -0,0 +1,64 @@
+// 1498. Number of Subsequences That Satisfy the Given Sum Condition
+// You are given an array of integers nums and an integer target.
+
+// Return the number of non-empty subsequences of nums such that the sum of the minimum and maximum element on it is less or equal to target. Since the answer may be too large, return it modulo 109 + 7.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,5,6,7], target = 9
+// Output: 4
+// Explanation: There are 4 subsequences that satisfy the condition.
+// [3] -> Min value + max value <= target (3 + 3 <= 9)
+// [3,5] -> (3 + 5 <= 9)
+// [3,5,6] -> (3 + 6 <= 9)
+// [3,6] -> (3 + 6 <= 9)
+// Example 2:
+
+// Input: nums = [3,3,6,8], target = 10
+// Output: 6
+// Explanation: There are 6 subsequences that satisfy the condition. (nums can have repeated numbers).
+// [3] , [3] , [3,3], [3,6] , [3,6] , [3,3,6]
+// Example 3:
+
+// Input: nums = [2,3,3,4,6,7], target = 12
+// Output: 61
+// Explanation: There are 63 non-empty subsequences, two of them do not satisfy the condition ([6,7], [7]).
+// Number of valid subsequences (63 - 2 = 61).
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// 1 <= nums[i] <= 106
+// 1 <= target <= 106
+
+
+class Solution {
+public:
+    int numSubseq(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int l = 0, r = n-1;
+
+        int MOD = 1000000007;
+        vector<int> power(n, 1);
+        for(int i = 1; i < n; i++) {
+            power[i] = (power[i-1]*2)%MOD;
+        }
+            
+
+        int ans = 0;
+        while(l <= r){
+            if(nums[l] + nums[r] <= target){
+                ans = (ans + power[r-l]) % MOD;
+                l++;
+            } else {
+                r--;
+            }
+        }
+
+        return ans;
+    }
+};
